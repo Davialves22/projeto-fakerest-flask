480619aea7cd12554726ab0f70035ac025c76574
davi gerson <dgas1@discente.ifpe.edu.br>
2025-10-29 21:20:50 -0300

    adicionado variavel de banco de dados e psycopg2

--- fakerest/__init__.py
+++ fakerest/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_manager, LoginManager
+import os
 
 # criando o app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "e85c8ede78483f3659f96991f107c440"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
